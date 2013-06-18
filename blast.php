9e20458165f967c63c57217380044e7754bb4bdc
Saif Al-Naib <salnaib@ryerson.ca>
2013-06-17 23:13:20 -0400

    mobile styling

--- blast.php
+++ blast.php
@@ -1,7 +1,22 @@
-<html xmlns="http://www.w3.org/1999/xhtml"> 
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
 <head>
-<meta http-equiv="content-type" content="text/html; charset=utf-8">
-<title>CrowdLinker - Back-End Developer</title>
+
+	<!-- Basic Page Needs
+  ================================================== -->
+	<meta charset="utf-8">
+	<title>Blast - Send Messages to your LinkedIn Contacts with One Click</title>
+
+	<!-- Mobile Specific
+  ================================================== -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<!-- CSS
+  ================================================== -->
+	<link rel="shortcut icon" href="./favicon.ico" type="image/x-icon" />
 <link type="text/css" rel="stylesheet" href="css/fcbklistselection.css" />
 <link type="text/css" rel="stylesheet" href="css/custom3.css" />
 <script type="text/javascript" async="" src="http://www.google-analytics.com/ga.js"></script>
